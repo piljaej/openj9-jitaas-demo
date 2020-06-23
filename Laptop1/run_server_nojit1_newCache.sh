349cc3ff3d76540fb06e76d6fd1567e99901502b
piljaej <54825962+piljaej@users.noreply.github.com>
2020-06-22 20:40:00 -0400

    Update run_server_nojit1_newCache.sh
    logging of newCache

--- Laptop1/run_server_nojit1_newCache.sh
+++ Laptop1/run_server_nojit1_newCache.sh
@@ -34,4 +34,5 @@ sudo docker run -d --rm \
     -p 192.168.0.16:9290:9290 \
     -v $PWD/sharedCache_openj9:/cache \
     -v $PWD/server_output_nojit1:/output \
+    --env JVM_ARGS="-Xshareclasses:controlDir=/cache,name=server_nojit -Xnoaot -XX:+UseContainerSupport -XX:+UseJITServerAddress=192.168.0.16 -Xjit:sampleThreshold=0,scorchingSampleThreshold=0,verbose={compilePerformance},vlog=/output/vlog,disableSuffixLogs,traceFull,log=/output/log_nojit1_newCache" \    
     --name server_nojit1 server_nojit1
