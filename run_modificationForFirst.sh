21bb7c0fec66d2c263378df9db45f23ea2d78f6c
piljaej <54825962+piljaej@users.noreply.github.com>
2020-06-24 15:14:07 -0400

    Update run_modificationForFirst.sh

--- run_modificationForFirst.sh
+++ run_modificationForFirst.sh
@@ -1,6 +1,8 @@
 #For first
 #Download original optimizer source code
 sudo rm -rf optimizer
+sudo docker stop openj9-jit
+sudo docker rm openj9-jit
 
 sudo docker run -d --name openj9-jit openj9-jitserver-build
 sudo docker cp openj9-jit:/root/openj9-openjdk-jdk8/omr/compiler/optimizer ./optimizer
@@ -12,4 +14,7 @@ sudo docker run -it \
 -v $PWD/build_compiler_only.sh:/root/build_compiler_only.sh \
 --name openj9-jit openj9-jitserver-build /bin/bash "./build_compiler_only.sh"
 
-docker commit openj9-jit openj9-jit-compiler
+sudo docker commit openj9-jit openj9-jit-compiler
+
+sudo docker stop openj9-jit
+sudo docker rm openj9-jit
