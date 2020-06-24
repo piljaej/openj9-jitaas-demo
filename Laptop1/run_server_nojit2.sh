fe19c5648155a8daf960ef50a490ea70485e6bf3
piljaej <54825962+piljaej@users.noreply.github.com>
2020-06-24 14:31:05 -0400

    Update run_server_nojit2.sh

--- Laptop1/run_server_nojit2.sh
+++ Laptop1/run_server_nojit2.sh
@@ -33,5 +33,6 @@ sudo docker run -d --rm \
     -p 192.168.0.16:9390:9390 \
     -v $PWD/sharedCache_openj9:/cache \
     -v $PWD/server_output_nojit2:/output \
+    -v $PWD/server_nojit2/j2sdk-image:/opt/openjdk8-openj9 \
     --env JVM_ARGS="-Xshareclasses:controlDir=/cache,name=server_nojit -Xnoaot -XX:+UseContainerSupport -XX:+UseJITServerAddress=192.168.0.16 -Xjit:sampleThreshold=0,scorchingSampleThreshold=0,verbose={compilePerformance},vlog=/output/vlog,disableSuffixLogs,traceFull,log=/output/log_nojit2" \
     --name server_nojit2 server_nojit2
