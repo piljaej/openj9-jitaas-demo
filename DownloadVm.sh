cd72622a67558e411dd7f738adfe4374b953e2b1
piljaej <54825962+piljaej@users.noreply.github.com>
2020-08-26 20:22:05 +0000

    Download OpenJ9 VM component and save to openj9_vm

---
+++ DownloadVm.sh
@@ -0,0 +1,6 @@
+sudo docker run -d --name openj9-jit openj9-jitserver-build
+sudo docker cp openj9-jit:/root/openj9-openjdk-jdk8/openj9/runtime/vm ./openj9_vm
+sudo docker commit openj9-jit openj9-vm-builder
+
+sudo docker stop openj9-jit
+sudo docker rm openj9-jit
