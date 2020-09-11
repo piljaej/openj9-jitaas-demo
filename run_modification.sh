6d6ec2bb053a58115b4ef451541e09a452b2b38b
piljaej <54825962+piljaej@users.noreply.github.com>
2020-09-11 17:49:15 +0000

    Update run_modification.sh

--- run_modification.sh
+++ run_modification.sh
@@ -1,14 +1,16 @@
 sudo docker run -it \
--v $PWD/optimizer:/root/openj9-openjdk-jdk8/omr/compiler/optimizer \
+-v $PWD/omr_compiler:/root/openj9-openjdk-jdk8/omr/compiler \
+-v $PWD/openj9_compiler:/root/openj9-openjdk-jdk8/openj9/runtime/compiler \
 -v $PWD/build_compiler_only.sh:/root/build_compiler_only.sh \
 --name openj9-jit-comp openj9-jit-compiler /bin/bash "./build_compiler_only.sh"
 
 rm -rf ./j2sdk-image
+rm -rf ../../j2sdk-image
 
 sdk_path_docker=/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image
 sudo docker cp openj9-jit-comp:$sdk_path_docker ./j2sdk-image
+sudo docker cp openj9-jit-comp:$sdk_path_docker ../../j2sdk-image
 sudo docker commit openj9-jit-comp openj9-jit-compiler
-
 
 rm -rf ./Laptop1/server_nojit1/j2sdk-image
 rm -rf ./Laptop1/server_nojit2/j2sdk-image
