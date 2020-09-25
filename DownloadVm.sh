7582ce94f3b08c27cd5916498162085f766314f7
piljaej <54825962+piljaej@users.noreply.github.com>
2020-09-25 06:21:10 +0000

    Update DownloadVm.sh

--- DownloadVm.sh
+++ DownloadVm.sh
@@ -1,3 +1,5 @@
+sudo rm -rf openj9_vm
+
 sudo docker run -d --name openj9-jit openj9-jitserver-build
 sudo docker cp openj9-jit:/root/openj9-openjdk-jdk8/openj9/runtime/vm ./openj9_vm
 sudo docker commit openj9-jit openj9-vm-builder
